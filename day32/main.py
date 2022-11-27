92709bc74e0a0c7b065788ffab6157484ac9f975
redrum88 <117243822+redrum88@users.noreply.github.com>
2022-11-27 16:48:46 +0000

    Day30 - Code Challenge: Quote EmailSender
    Quote Email Sender will send email to receiver email with random quote chosen from quotes.txt if current week of the day is the one set in variables.
    
    Easy change to own need.
    
    Worked well with gmail.com
    
    *Note password need obtain App Password from you google account which looks like "abcd abcd abcd abcd" copy and in main.py paste like this  my_password = "abcdabcdabcdabcd"

---
+++ day32/main.py
@@ -0,0 +1,34 @@
+import smtplib
+import datetime as dt
+import random
+
+# Global variables
+file = "quotes_texts.txt"     # File to read. Each line of text will be randomly chosen and used as email body text.
+weekday_to_check = 6    # Set day of week to check: 0 for Monday to 6 for Sunday
+my_email = "your@email.com"     # Enter your email address here
+my_password = "your_password"    # Enter your password here
+receiver = "receiver@email.com"     # Enter recipient email address
+smtp_server = "smtp.gmail.com"      # SMTP Server Address
+smtp_port = 587                     # SMTP Server Port
+subject_title = "Motivation Quote:"      # Change to your subject title.
+
+# Use the datetime module to obtain the current day of the week
+now = dt.datetime.now()
+weekday = now.weekday()
+
+# Check if current day of the week is the is set as default.
+if weekday == weekday_to_check:
+    # Obtain the file.txt file and obtain a list of text
+    with open(file, "r") as f:
+        lines = f.readlines()
+        random_line = random.choice(lines)
+
+    # Send email with random text.
+    with smtplib.SMTP(smtp_server, port=smtp_port) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=receiver,
+            msg=f"Subject:{subject_title}:\n\n{random_line}"
+        )
