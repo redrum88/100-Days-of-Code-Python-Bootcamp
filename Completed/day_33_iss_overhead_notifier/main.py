efdd64a0ff2218680e98d72a0d5d6f4680c8a9a9
redrum88 <117243822+redrum88@users.noreply.github.com>
2022-11-28 12:50:50 +0000

    Add Day 33
    *ISS Overhead Notifier
    *Kanye Quotes

---
+++ Completed/day_33_iss_overhead_notifier/main.py
@@ -0,0 +1,59 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = "email@gmail.com"  # Enter your email address here
+MY_PASSWORD = "yourpassword"  # Enter your password here
+SMTP_HOST = "smtp.gmail.com"  # SMTP Server Address
+SMTP_PORT = 587
+MY_LAT = 51.507351  # Your latitude
+MY_LONG = -0.127758  # Your longitude
+
+
+def is_iss_overhead():
+    iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+    data = iss_response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP(SMTP_HOST)
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject: Look Up\n\nThe ISS is above you in the sky."
+        )
+
+# If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
