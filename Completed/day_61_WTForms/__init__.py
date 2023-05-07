a8476030f1316ac7298dd91376430d7777ac6858
redrum88 <117243822+redrum88@users.noreply.github.com>
2023-05-08 00:26:27 +0100

    day 61 flask WTForms

---
+++ Completed/day_61_WTForms/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py this file will install all required packages before running the app
+import os
+
+os.system('pip install -r requirements.txt')
+os.system('python main.py')
---
+++ Completed/day_61_WTForms/website/__init__.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+# PostgreSQL database connection
+db = SQLAlchemy()
+DB_NAME = "wtforms"
+host = "localhost"
+port = "5432"
+username = "postgres"
+password = "Naujas293"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{username}:{password}@{host}:{port}/{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import Users, Note
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Users.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
